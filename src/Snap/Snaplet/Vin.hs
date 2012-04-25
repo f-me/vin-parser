0d4138fca02df780ea2a810c9d545ee7b7601a79
Tim <tymmym@gmail.com>
2012-04-26 03:18:52 +0400

    Send alerts in JSON

--- src/Snap/Snaplet/Vin.hs
+++ src/Snap/Snaplet/Vin.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Snap.Snaplet.Vin
     ( Vin
@@ -13,13 +14,16 @@ import           Control.Concurrent
 import qualified Control.Exception as E
 import           Control.Monad.IO.Class
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
 import           Data.Map (Map)
 import           Data.Map as M
 import           Data.Maybe
+import           GHC.Generics
 import           System.Directory (getTemporaryDirectory)
 
 import           Control.Monad.CatchIO (finally, catch)
 import           Control.Monad.State
+import           Data.Aeson
 import           Data.Lens.Template
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -31,11 +35,56 @@ import           System.Posix.Files (createLink, removeLink)
 import           Vin.Import
 
 
+data Alert a = Alert
+    { alertId        :: a
+    , alertType      :: Text
+    , alertMessage   :: Text
+    , alertVinFile   :: ByteString
+    , alertErrorFile :: Maybe FilePath
+    } deriving (Show, Eq, Generic)
+
+
+instance ToJSON a => ToJSON (Alert a)
+
+
+newtype Alerts a = Alerts (Map a (Alert a))
+
+
+instance ToJSON a => ToJSON (Alerts a) where
+    toJSON (Alerts m) = object [("alerts", as)]
+        where
+          as = toJSON $ M.foldrWithKey f [] m
+          f k v res = v { alertId = k } : res
+
+
+insertAlert :: Ord a => Alert a -> Alerts a -> Alerts a
+insertAlert a (Alerts m) =
+    Alerts $ M.insert (alertId a) a m
+
+
+
+updateAlert :: Ord a => Alert a -> Alerts a -> Alerts a
+updateAlert a (Alerts m) =
+    Alerts $ M.update (const $ Just a) (alertId a) m
+
+
+deleteAlert :: Ord a => a -> Alerts a -> Alerts a
+deleteAlert k (Alerts m) =
+    Alerts $ M.delete k m
+
+
+------------------------------------------------------------------------------
 data Vin = Vin
-    { _state :: MVar (Map String Text)
+    { _alerts :: MVar (Alerts ByteString)
     }
 
+
 makeLens ''Vin
+
+
+modifyMVar' :: MVar a -> (a -> a) -> IO ()
+modifyMVar' m f =
+    modifyMVar_ m (return . f)
 
 
 ------------------------------------------------------------------------------
@@ -63,30 +112,37 @@ action :: ByteString -> PartInfo -> String -> Handler b Vin ()
 action program info f = do
     liftIO $ createLink f f'
 
-    s <- gets _state
+    s <- gets _alerts
 
-    liftIO $ modifyMVar_ s $ \m ->
-        return $ M.insert f "Обработка..." m
+    let a = Alert (B.pack f)
+                  "info"
+                  "обработка..."
+                  (fromMaybe "" $ partFileName info)
+                  Nothing
+
+    liftIO $ modifyMVar' s $ insertAlert a
 
     liftIO $ forkIO $ do
         loadFile f' fError program $ partContentType info
 
-        modifyMVar_ s $ \m ->
-            return $ M.update (const $ Just "Закончено!") f m
+        modifyMVar' s $ updateAlert a { alertType = "success"
+                                     , alertMessage = "закончено"
+                                     }
 
         `E.catches` [ E.Handler (\ (ex :: VinUploadException) ->
-                                     modifyMVar_ s $ \m -> do
-                                         return $ M.update (const . Just $ vReason ex) f m
+                                     modifyMVar' s $ updateAlert a { alertType = "error"
+                                                                   , alertMessage = vReason ex
+                                                                   , alertErrorFile = vFilePath ex }
                                 )
                     , E.Handler (\ (ex :: E.SomeException) ->
-                                     modifyMVar_ s $ \m -> do
-                                         return $ M.update (const . Just $  T.pack $ show ex) f m
+                                     modifyMVar' s $ updateAlert a { alertType = "error"
+                                                                   , alertMessage = T.pack $ show ex }
                                 )
                     ]
 
         `finally` removeLink f'
 
-    writeBS "Ok" -- send to client
+    writeBS "Ok"
   where
     fError = "resources/static/error.csv"
     f' = f ++ "-link"
@@ -94,27 +150,36 @@ action program info f = do
 
 getState :: Handler b Vin ()
 getState = do
-    s <- gets _state
-    res <- liftIO $ readMVar s
-    putResponse $ setContentType "text/html; charset=utf-8" $ emptyResponse
+    s <- gets _alerts
+    as <- liftIO $ readMVar s
+    writeLBS $ encode as
 
-    mapM_ (\(_,msg) -> writeText $ T.concat [ "<div class='alert alert-info'>\
-                                              \<a class='close' data-dismiss='alert'>x</a>"
-                                            , msg
-                                            , "</div>"
-                                            ]
-          ) $ M.toList res
+
+removeAlert :: Handler b Vin ()
+removeAlert = do
+    s <- gets _alerts
+
+    res <- getParam "id"
+
+    liftIO $ case res of
+               Nothing -> return ()
+               Just k  -> f s k
+
+  where
+    f s k = modifyMVar_ s $ \as ->
+                  return $ deleteAlert k as
 
 
 routes :: [(ByteString, Handler b Vin ())]
 routes = [ ("/upload", method POST upload)
-         , ("/state", method POST getState)
+         , ("/state", method GET getState)
+         , ("/state", method POST removeAlert)
          ]
 
 
 vinInit :: SnapletInit b Vin
 vinInit =
     makeSnaplet "vin" "Some description" Nothing $ do
-        s <- liftIO $ newMVar M.empty
+        as <- liftIO . newMVar $ Alerts M.empty
         addRoutes routes
-        return $ Vin s
+        return $ Vin as
